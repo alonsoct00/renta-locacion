33f6f74f003e6cec2b3fe7e2013cf77f2f1ea0b2
Alonso CT <g.alonso.ct@gmail.com>
2024-07-26 09:00:54 -0600

    add language support

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,10 +6,14 @@
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
         <b-nav-item href="#home" @click.prevent="scrollTo('home')" class="nav-item">{{ $t('navbar.home') }}</b-nav-item>
-        <b-nav-item href="#amenities" @click.prevent="scrollTo('amenities')" class="nav-item">{{ $t('navbar.amenities') }}</b-nav-item>
-        <b-nav-item href="#local-guide" @click.prevent="scrollTo('local-guide')" class="nav-item">{{ $t('navbar.localGuide') }}</b-nav-item>
-        <b-nav-item href="#gallery" @click.prevent="scrollTo('gallery')" class="nav-item">{{ $t('navbar.gallery') }}</b-nav-item>
-        <b-nav-item href="#contact" @click.prevent="scrollTo('contact')" class="nav-item">{{ $t('navbar.contact') }}</b-nav-item>
+        <b-nav-item href="#amenities" @click.prevent="scrollTo('amenities')" class="nav-item">{{ $t('navbar.amenities')
+          }}</b-nav-item>
+        <b-nav-item href="#local-guide" @click.prevent="scrollTo('local-guide')" class="nav-item">{{
+          $t('navbar.localGuide') }}</b-nav-item>
+        <b-nav-item href="#gallery" @click.prevent="scrollTo('gallery')" class="nav-item">{{ $t('navbar.gallery')
+          }}</b-nav-item>
+        <b-nav-item href="#contact" @click.prevent="scrollTo('contact')" class="nav-item">{{ $t('navbar.contact')
+          }}</b-nav-item>
         <!----<b-nav-item @click="showPoliciesModal" class="nav-item">{{ $t('navbar.policies') }}</b-nav-item>-->
       </b-navbar-nav>
     </b-collapse>
@@ -47,22 +51,33 @@ export default {
 
 <style scoped>
 .navbar-custom {
-  background-color: #343a40; /* Color de fondo del navbar */
-  box-shadow: 0 2px 4px rgba(0,0,0,0.1); /* Sombra */
+  background-color: #343a40;
+  /* Color de fondo del navbar */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  /* Sombra */
 }
 
 .navbar-brand {
-  color: #ffffff; /* Color del texto del brand */
-  font-size: 1.5rem; /* Tamaño de fuente */
+  color: #ffffff;
+  /* Color del texto del brand */
+  font-size: 1.5rem;
+  /* Tamaño de fuente */
 }
 
 .nav-item {
-  color: #ffffff !important; /* Color del texto de los ítems */
-  font-size: 1rem; /* Tamaño de fuente */
+  color: #ffffff !important;
+  /* Color del texto de los ítems */
+  font-size: 1rem;
+  /* Tamaño de fuente */
 }
 
 .nav-item:hover,
 .nav-item:focus {
-  background-color: rgba(255, 255, 255, 0.1); /* Color de fondo al pasar el mouse o hacer foco */
+  background-color: rgba(255, 255, 255, 0.1);
+  /* Color de fondo al pasar el mouse o hacer foco */
+}
+
+.nav-link {
+  color: #ffffff;
 }
 </style>
